c99e57475bfc99da2ec3df39f56e384e47680618
sujaldidwaniya <sujaldidwaniya87@gmail.com>
2026-09-09 19:35:36 +0530

    Secure Django settings for production

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4=@0+fp_pr_(e!4$gx&_q4!m7(n-^a-7q#spoyyyhc+=dn!dng"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
